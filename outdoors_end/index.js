ad3e58d7bd2776c3f826ec1465961bef76e88744
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-12-04 12:13:25 -0300

    correccion en el index

--- outdoors_end/index.js
+++ outdoors_end/index.js
@@ -363,8 +363,8 @@ app.get("/saleshome", async (req, res) => {
 
 // Manejo de errores 404
 app.use((req, res, next) => {
-  res.status(404).json({
-    error: "Lo sentimos, recurso no encontrado. ¡Intenta otra vez!",
-    error,
+    res.status(404).json({
+      message: "Lo sentimos, recurso no encontrado. ¡Intenta otra vez!",
+      error: "Recurso no encontrado",
+    });
   });
-});
